b9d50cd2ffde390ad8a91d88644efa19c2495631
kaveenimsath <119869614+kaveenimsath@users.noreply.github.com>
2023-02-02 23:46:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -20,7 +20,7 @@ global.userRegister = false
 global.autoblok212 = false //ubah jadi true biar nmr 212 di blok sama bot
 
 //language
-//Available in indonesia , english & spanyol 
+//Available in  english  , english
 //You can request others language 
 global.language = english  //change indonesia to english if you don't understand the language used by the bot
 
@@ -28,17 +28,17 @@ global.language = english  //change indonesia to english if you don't understand
 // Other
 // Other
 global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ RASHMIKA ×፝֟͜×" //nama kalian
+global.ownername= "ᴹᴿ᭄ Nimsath ×፝֟͜×" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "janith20062006@gmail.com" //bebas
+global.email = "kaveenimsath67@gmail.com" //bebas
 global.region = "Sri Lanka" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['94717775628'] //premium user
-global.owner = ["94717775628"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "94717775628" // nomor wa kalian
-global.ownernomerr = "+94717775628" //nmr wa kalian
+global.timezone = 'Asia/colombo' //  timezone wib
+global.premium = ['94773745965'] //premium user
+global.owner = ["94773745965"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "94773745965" // nomor wa kalian
+global.ownernomerr = "+94773745965" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
